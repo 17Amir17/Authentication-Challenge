fe15d9ba803c63f7bce437e0ff3d6f77d0f73838
17Amir17 <amirangel101@hotmail.com>
2021-11-30 13:49:07 +0200

    Create data.js

---
+++ back/data/data.js
@@ -0,0 +1,12 @@
+const USERS = [
+  {
+    email: 'admin@email.com',
+    name: 'admin',
+    password: '**hashed password**',
+    isAdmin: true,
+  },
+];
+const INFORMATION = [{ email, info }];
+const REFRESHTOKENS = [];
+
+module.exports = { USERS, INFORMATION, REFRESHTOKENS };
