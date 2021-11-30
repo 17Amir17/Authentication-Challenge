bf0cb4fa6e05810eac89046f93c9340b5f1e4452
17Amir17 <amirangel101@hotmail.com>
2021-11-30 17:21:05 +0200

    unprettify

--- tests/AuthUser.test.js
+++ tests/AuthUser.test.js
@@ -3,43 +3,45 @@ const server = require('../app');
 
 // authorized user tests
 const userInfoMock = {
-  name: 'test',
-  email: 'test@test.com',
-  password: 'Aa123456!',
-};
+    name: 'test',
+    email: 'test@test.com',
+    password: 'Aa123456!'
+}
 
 describe('Authorized User Tests', () => {
-  afterAll(async () => {
-    await server.close();
-  });
+    afterAll(async () => {
+        await server.close();
+    })
 
-  //authorized user can get info
-  test('Authorized User Can Get Info', async (done) => {
-    const registerResponse = await request(server)
-      .post('/users/register')
-      .send(userInfoMock);
+    //authorized user can get info
+    test('Authorized User Can Get Info', async (done) => {
 
-    expect(registerResponse.status).toBe(201);
+        const registerResponse = await request(server)
+            .post('/users/register')
+            .send(userInfoMock)
 
-    const loginRes = await request(server)
-      .post('/users/login')
-      .send(userInfoMock);
+        expect(registerResponse.status).toBe(201)
 
-    expect(loginRes.status).toBe(200);
+        const loginRes = await request(server)
+            .post('/users/login')
+            .send(userInfoMock)
 
-    const infoRes = await request(server)
-      .get('/api/v1/information')
-      .set('authorization', `bearer ${loginRes.body.accessToken}`);
+        expect(loginRes.status).toBe(200)
 
-    expect(infoRes.status).toBe(200);
-    expect(infoRes.body.length > 0).toBe(true);
-    expect(infoRes.body[0].email).toBe(userInfoMock.email);
+        const infoRes = await request(server)
+            .get('/api/v1/information')
+            .set('authorization', `bearer ${loginRes.body.accessToken}`)
 
-    const informationResponse = await request(server)
-      .get('/api/v1/information')
-      .set('authorization', 'bearer notValidToken');
+        expect(infoRes.status).toBe(200)
+        expect(infoRes.body.length > 0).toBe(true)
+        expect(infoRes.body[0].email).toBe(userInfoMock.email)
 
-    expect(informationResponse.status).toBe(403);
-    done();
-  });
-});
+        const informationResponse = await request(server)
+            .get('/api/v1/information')
+            .set('authorization', 'bearer notValidToken')
+
+        expect(informationResponse.status).toBe(403)
+        done()
+    })
+})
+
